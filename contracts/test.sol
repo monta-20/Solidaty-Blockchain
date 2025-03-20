30b1df8a8816c44ef0ba81fc8308b802de5a45c5
monta-20 <montassarbenbrahim29@gmail.com>
2025-03-20 17:03:53 +0100

    the types

--- contracts/test.sol
+++ contracts/test.sol
@@ -2,12 +2,38 @@
 pragma solidity ^0.8.13;
 
 contract test {
-    uint nombre ; 
-    function getNomber() public view returns(uint){
-        return nombre ;
-    }
-    function setNomber(uint _nombre) public {
-        nombre = _nombre ;
+    uint public myUint;            // Unsigned integer
+    int public myInt;              // Signed integer
+    bool public isActive;          // Boolean
+    address public userAddress;    // Regular Ethereum address
+    address payable public wallet; // Payable Ethereum address
+    string private name ;          //String
+
+    // Function to set uint and int values
+    function setNumbers(uint _uintValue, int _intValue) public {
+        myUint = _uintValue;
+        myInt = _intValue;
     }
 
+    // Function to set boolean value
+    function setBoolean(bool _status) public {
+        isActive = _status;
+    }
+
+    // Function to set a regular Ethereum address
+    function setUserAddress(address _userAddr) public {
+        userAddress = _userAddr;
+    }
+
+    // Function to set a payable Ethereum address
+    function setWalletAddress(address payable _walletAddr) public {
+        wallet = _walletAddr;
+    }
+    //String
+   function setName(string memory _Newname) public {
+            name = _Newname;
+   }
+   function getName() public view returns (string memory) {
+    return name ;
+   }
 }
